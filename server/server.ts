dc815c5f6c1f75804170a1a608c4cc734cff09e2
John Evans <john.wes.evans@gmail.com>
2019-09-14 16:04:30 -0500

    ref https://www.youtube.com/watch?v=Ad3fj9V7s6A

--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,7 @@ const express = require('express');
 
 const app = express();
 
+
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
